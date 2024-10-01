6db3c130b66a0bb2bfffa284fe878c24665d307f
Danilo Belato <danilo.belato@alunos.unis.edu.br>
2024-09-30 22:22:17 -0300

    New commit teste

--- src/main/java/org/springBoot/PrimeirosPassosApplication.java
+++ src/main/java/org/springBoot/PrimeirosPassosApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
-
+class PrimeirosPassosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeirosPassosApplication.class, args);
+    }
+}
     public static void main(String[] args) {
         SpringApplication.run(PrimeirosPassosApplication.class, args);
     }
