5ca729d20632634f8172b6eb2d3baf0c811cba0a
Danilo Belato <danilo.belato@alunos.unis.edu.br>
2024-10-02 21:51:52 -0300

    New commit teste

--- src/main/java/org/springBoot/PrimeirosPassosApplication.java
+++ src/main/java/org/springBoot/PrimeirosPassosApplication.java
@@ -1,17 +1,18 @@
 package org.springBoot;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
-class PrimeirosPassosApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PrimeirosPassosApplication.class, args);
     }
-}
-    public static void main(String[] args) {
-        SpringApplication.run(PrimeirosPassosApplication.class, args);
-    }
+    calculadora calculadora = new calculadora();
+
+  System.out.println("ok");
+
 
 }
---
+++ src/main/java/org/springBoot/calculadora.java
@@ -0,0 +1,7 @@
+package org.springBoot;
+
+public class calculadora {
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
